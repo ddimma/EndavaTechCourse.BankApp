d7eff6b65fef4c91dfa4fdec2f51f0362163fab0
“ddimma” <dumitru.pavlov@mib.utm.md>
2023-10-16 21:00:40 +0300

    Setting up MudBlazor package and basic layout

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using EndavaTechCourse.BankApp.Client;
+using MudBlazor.Services;
 
 namespace EndavaTechCourse.BankApp.Client;
 
@@ -11,6 +12,8 @@ public class Program
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddMudServices();
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
