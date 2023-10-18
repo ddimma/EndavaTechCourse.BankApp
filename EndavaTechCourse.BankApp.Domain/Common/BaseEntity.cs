c7b91f76f5cb5f3ad8a5c4e3adad815a852e3ac0
“ddimma” <dumitru.pavlov@mib.utm.md>
2023-10-18 18:39:36 +0300

    Adding Domanin Layer

---
+++ EndavaTechCourse.BankApp.Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EndavaTechCourse.BankApp.Domain.Common
+{
+	public class BaseEntity
+	{
+		public BaseEntity()
+		{
+		}
+	}
+}
+
---
+++ EndavaTechCourse.BankApp.Domain/Models/Wallet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EndavaTechCourse.BankApp.Domain.Models
+{
+	public class Wallet
+	{
+		public Wallet()
+		{
+		}
+	}
+}
+
